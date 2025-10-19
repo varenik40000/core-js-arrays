04abaa041b43d11023a2067e4888749390dd8232
varenik40000 <64190191+varenik40000@users.noreply.github.com>
2025-10-19 23:08:14 +0500

    feat: add desicion

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,11 @@ function getIntervalArray(/* start, end */) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  if (arr1.length > arr2.length) {
+    return arr1.map((el, index) => el + (arr2[index] ? arr2[index] : 0));
+  }
+  return arr2.map((el, index) => el + (arr1[index] ? arr1[index] : 0));
 }
 
 /**
@@ -53,8 +56,8 @@ function sumArrays(/* arr1, arr2 */) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value);
 }
 
 /**
@@ -71,8 +74,14 @@ function findElement(/* arr, value */) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  const initialValue = 0;
+  const sum = arr.reduce(
+    (accumulator, currentValue) =>
+      currentValue === item ? accumulator + 1 : accumulator + 0,
+    initialValue
+  );
+  return sum;
 }
 
 /**
